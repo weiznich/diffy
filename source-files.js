5b282735b2fec406c55be6cae1d295396fd724bd
bmwill <bmwill@users.noreply.github.com>
2020-07-02 01:51:38 +0000

    deploy: fe09ab83f16f87e3f8f2ed658aed18e88e05890d

---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["diffy"] = {"name":"","dirs":[{"name":"diff","files":["cleanup.rs","mod.rs","myers.rs"]},{"name":"merge","files":["mod.rs"]},{"name":"patch","files":["format.rs","mod.rs","parse.rs"]}],"files":["apply.rs","lib.rs","range.rs","utils.rs"]};
+createSourceSidebar();
